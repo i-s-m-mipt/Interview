a060c8cabc63b0ca89f492c5459495eb763aa105
190924 <i.s.m.mipt@yandex.ru>
2019-11-20 16:17:09 +0300

    Add project files.

---
+++ initi/source.cpp
@@ -0,0 +1,44 @@
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+namespace initi
+{
+	class Container // based on RB tree
+	{
+	private:
+
+
+
+	public:
+
+	private:
+
+	};
+
+} // namespace initi
+
+int main(int argc, char * argv[])
+{
+	try
+	{
+		
+
+		system("pause");
+
+		return EXIT_SUCCESS;
+	}
+	catch (const std::exception & exception)
+	{
+		std::cerr << "Fatal error: " << exception.what() << std::endl;
+
+		return EXIT_FAILURE;
+	}
+	catch (...)
+	{
+		std::cerr << "Fatal error: " << "unknown exception" << std::endl;
+
+		return EXIT_FAILURE;
+	}
+}
